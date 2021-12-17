3fde2553bee806a221e51b3ac4609e2b54dd2dbc
Ibtisam <ibtisam.gull@gmail.com>
2021-12-17 11:37:50 +0500

    Updated

--- app/src/main/java/com/example/manifestdeclaredreceiver/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/manifestdeclaredreceiver/MyBroadcastReceiver.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Message from Broadcast: "+ intent.getStringExtra("MESS"), Toast.LENGTH_SHORT).show();
+
     }
 }
